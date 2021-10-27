4480c06a64d081f4e33f1170159b0c5686325c0f
Savitsky Evgeni <zheni11seg@gmail.com>
2021-10-27 11:03:30 +0300

    create create-method to AuthorDao

--- src/main/java/com/epam/jwd/library/dao/AuthorDao.java
+++ src/main/java/com/epam/jwd/library/dao/AuthorDao.java
@@ -2,23 +2,25 @@ package com.epam.jwd.library.dao;
 
 import com.epam.jwd.library.connection.ConnectionPool;
 import com.epam.jwd.library.entity.Author;
-import com.epam.jwd.library.entity.Entity;
 import com.epam.jwd.library.exception.AuthorNotFoundException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 public class AuthorDao extends AbstractDao<Author>{
 
     private static final Logger LOG = LogManager.getLogger(AuthorDao.class);
+
     public static final String SELECT_ALL_AUTHORS = "select id as id, first_name as f_name, last_name as l_name from author";
+    public static final String LAST_NAME_COLUMN_NAME = "l_name";
+    public static final String FIRST_NAME_COLUMN_NAME = "f_name";
+    public static final String ID_COLUMN_NAME = "id";
+    public static final String INSERT_NEW_AUTHOR = "insert into author (first_name, last_name) values (?, ?)";
 
     public AuthorDao(ConnectionPool pool) {
         super(pool, LOG);
@@ -26,6 +28,8 @@ public class AuthorDao extends AbstractDao<Author>{
 
     @Override
     public boolean create(Author entity) {
+        final Connection connection = pool.takeConnection();
+        final PreparedStatement preparedStatement = connection.prepareStatement(INSERT_NEW_AUTHOR);
         return false;
     }
 
@@ -41,7 +45,7 @@ public class AuthorDao extends AbstractDao<Author>{
              final Statement statement = connection.createStatement();
              final ResultSet resultSet = statement.executeQuery(SELECT_ALL_AUTHORS)){
             while (resultSet.next()) {
-                final Author author = executeAuthor(resultSet).orElseThrow(() -> new AuthorNotFoundException(""));
+                final Author author = executeAuthor(resultSet).orElseThrow(() -> new AuthorNotFoundException("could not extract author"));
                 authors.add(author);
             }
         } catch (SQLException e) {
@@ -49,13 +53,13 @@ public class AuthorDao extends AbstractDao<Author>{
         } catch (AuthorNotFoundException e) {
             LOG.error("did not found authors", e);
         }
-        return null;
+        return Collections.emptyList();
     }
 
     private Optional<Author> executeAuthor(ResultSet resultSet){
         try {
-            return Optional.of(new Author(resultSet.getLong("id"), resultSet.getString("f_name"),
-                    resultSet.getString("l_name")));
+            return Optional.of(new Author(resultSet.getLong(ID_COLUMN_NAME), resultSet.getString(FIRST_NAME_COLUMN_NAME),
+                    resultSet.getString(LAST_NAME_COLUMN_NAME)));
         } catch (SQLException e) {
             LOG.error("could not extract author from executeAuthor", e);
             return Optional.empty();
